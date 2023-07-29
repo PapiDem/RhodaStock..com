5723c2285525cd034ac8c99bf9f81e5b8e7077e4
PapiDem <140867635+PapiDem@users.noreply.github.com>
2023-07-29 09:24:32 -0700

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+	$(".fa-bars").click(function(){
+		$(".menu").animate({left:270},600)
+		$(".text_1").animate({left;"770px"},600)
+		$(".fa-close").css("visibility" , "visible")
+		$(".fa-close").animate({left:"270px"},600)
+		$(".fa-bars").css("visibility" , "visible")
+	}
+}
